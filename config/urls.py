88b187476dd9b17fc4a2ed43800e7ba3153f2b9b
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-09-09 22:44:09 +0300

    add BookViewSet and urls

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("api/", include("books.urls", namespace="books")),
 ]
