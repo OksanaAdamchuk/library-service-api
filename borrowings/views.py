f2df803b19c559231c8e18d3ff2862ccacfdd1d2
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-09-11 02:49:34 +0300

    add permission for list and retrieve actions

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,20 +1,44 @@
-from rest_framework import generics, serializers
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from django.db.models import QuerySet
+from django.shortcuts import get_object_or_404
 
 from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingDetailSerializer, BorrowingListSerializer, CreateBorrowingSerializer
 
 
 class BorrowingListView(generics.ListCreateAPIView):
-    queryset = Borrowing.objects.select_related("book", "user")
     serializer_class = BorrowingListSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
         if self.request.method == "POST":
             return CreateBorrowingSerializer
         elif self.request.method == "GET":
             return BorrowingListSerializer
+    
+    def get_queryset(self):
+        queryset = Borrowing.objects.select_related("book", "user")
+
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+
+        return queryset
 
 
 class BorrowingRetrieveView(generics.RetrieveAPIView):
-    queryset = Borrowing.objects.select_related("book", "user")
     serializer_class = BorrowingDetailSerializer    
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Borrowing.objects.select_related("book", "user")
+        if self.request.user.is_staff:
+            return queryset
+        else:
+            return Borrowing.objects.filter(user=self.request.user)
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset, pk=self.kwargs["pk"])
+        self.check_object_permissions(self.request, obj)
+        return obj
