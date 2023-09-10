d1ab46b63cc191b55ec77af31d75301197281143
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-09-11 01:00:10 +0300

    add borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,35 @@
+from datetime import date
+from django.conf import settings
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField(
+        validators=[MinValueValidator(limit_value=date.today)]
+    )
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.PROTECT, related_name="borrowings")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="borrowings"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.user.first_name} {self.user.last_name} borrows {self.book.title} till {self.expected_return_date}"
+
+    class Meta:
+        ordering = ["borrow_date"]
+
+    def full_clean(self, exclude=None, validate_unique=True):
+        super().full_clean(exclude=exclude, validate_unique=validate_unique)
+
+        if self.actual_return_date < self.borrow_date:
+            raise ValidationError("You can't return book before the date you've borrowed it.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
