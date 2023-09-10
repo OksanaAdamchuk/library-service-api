c44e856720dec882bb5eec34b8961073206eb91a
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-09-10 22:46:45 +0300

    add endpoints for updating and viewing profile info

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from users.views import CreateUserView
+from users.views import CreateUserView, ManageUserView
 
 
 app_name = "users"
@@ -13,4 +13,5 @@ urlpatterns = [
     path("users/", CreateUserView.as_view(), name="create"),
     path("users/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("users/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("users/me/", ManageUserView.as_view(), name="manage"),
 ]
