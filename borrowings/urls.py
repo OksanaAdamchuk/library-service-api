cd505d6b3092f05a7ffa453b6229ab2d0b369e7a
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-09-15 21:05:11 +0300

    change separate views of Borrowing model to ViewSet

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,22 +1,11 @@
-from django.urls import path
+from rest_framework import routers
 
-from borrowings.views import (
-    BorrowingListView,
-    BorrowingRetrieveView,
-    ReturnBorrowingView,
-)
+from borrowings.views import BorrowingViewSet
 
 
 app_name = "borrowings"
 
-urlpatterns = [
-    path("borrowings/", BorrowingListView.as_view(), name="borrowing-list-create"),
-    path(
-        "borrowings/<int:pk>/", BorrowingRetrieveView.as_view(), name="borrowing-detail"
-    ),
-    path(
-        "borrowings/<int:pk>/return/",
-        ReturnBorrowingView.as_view(),
-        name="return-borrowing",
-    ),
-]
+router = routers.DefaultRouter()
+router.register("borrowings", BorrowingViewSet, basename="borrowings")
+
+urlpatterns = router.urls
